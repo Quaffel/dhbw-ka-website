e498bfdc35f546a9195d1d8afdbf1a8b663c046c
BottledSoda <linkkay123@gmail.com>
2021-02-09 13:56:34 +0100

    refactored image components to one component

--- frontend/src/components/mdx/rect-Image/rectImage.tsx
+++
@@ -1,7 +0,0 @@
-import './rectImage.scss';
-
-import React from "react";
-
-export function RectImage({ src, alt }: { src: string, alt: string }) {
-  return <img src={src} className={"image-rect image-full"} alt={alt}/>
-}
