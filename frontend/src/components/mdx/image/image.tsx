9edd6e0bfefa27d93a00e6dbdfaf8a3424f2ac04
BottledSoda <linkkay123@gmail.com>
2021-02-11 18:28:24 +0100

    renamed "shape" to "props"

--- frontend/src/components/mdx/image/image.tsx
+++ frontend/src/components/mdx/image/image.tsx
@@ -2,6 +2,6 @@ import './image.scss';
 
 import React from "react";
 
-export function Image({ src, shape, alt }: { src: string, shape: string, alt: string }) {
-  return <img src={src} className={shape} alt={alt}/>
+export function Image({ src, props, alt }: { src: string, props: string, alt: string }) {
+  return <img src={src} className={props} alt={alt}/>
 }
