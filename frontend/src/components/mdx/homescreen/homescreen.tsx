d5022ac7ebd908f9ba0eba734601c8ccebec6ce7
BottledSoda <linkkay123@gmail.com>
2021-02-12 12:12:19 +0100

    fixed homescreen

--- frontend/src/components/mdx/homescreen/homescreen.tsx
+++ frontend/src/components/mdx/homescreen/homescreen.tsx
@@ -4,9 +4,8 @@ import { TextField } from 'components/mdx/textField/textField';
 
 import React from "react";
 
-export function Homescreen({ src, props, alt, text }: { src: string, props: string, alt: string, text: string }) {
+export function Homescreen({ props, text }: { src: string, props: string, alt: string, text: string }) {
     return <div className="homeContainer">
-        <Image src ={src} props = { props }  alt = { alt} />
         <TextField text = {text} props = {"centered"}/>
         </div>
   }
