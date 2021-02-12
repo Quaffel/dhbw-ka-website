bc1f9bf85bf7dcd3ef7d3d3e72ca279cf9549f59
BottledSoda <linkkay123@gmail.com>
2021-02-12 10:13:25 +0100

    renamed container

--- frontend/src/components/mdx/homescreen/homescreen.tsx
+++ frontend/src/components/mdx/homescreen/homescreen.tsx
@@ -1,11 +1,12 @@
 import './homescreen.scss';
 import { Image } from 'components/mdx/image/image';
+import { TextField } from 'components/mdx/textField/textField';
 
 import React from "react";
 
 export function Homescreen({ src, props, alt, text }: { src: string, props: string, alt: string, text: string }) {
-    return <div className="container">
+    return <div className="homeContainer">
         <Image src ={src} props = { props }  alt = { alt} />
-        <div className="centered"><b>{text}</b></div>
+        <TextField text = {text} props = {"centered"}/>
         </div>
   }
