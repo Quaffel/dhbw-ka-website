233d44c70db8d3528652e40cd7a2b6bbe2d8b916
BottledSoda <linkkay123@gmail.com>
2021-02-12 09:13:31 +0100

    inserting adapting text (no real design yet)

--- frontend/src/components/mdx/homescreen/homescreen.tsx
+++ frontend/src/components/mdx/homescreen/homescreen.tsx
@@ -3,9 +3,9 @@ import { Image } from 'components/mdx/image/image';
 
 import React from "react";
 
-export function Homescreen({ src, props, alt }: { src: string, props: string, alt: string }) {
+export function Homescreen({ src, props, alt, text }: { src: string, props: string, alt: string, text: string }) {
     return <div className="container">
         <Image src ={src} props = { props }  alt = { alt} />
-        <div className="centered"><b>Hier könnte ihre Werbunge stehen</b></div>
+        <div className="centered"><b>{text}</b></div>
         </div>
   }
