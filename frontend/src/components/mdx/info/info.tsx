cde54049f97b6837759d3e23da1fc329b1750008
BottledSoda <linkkay123@gmail.com>
2021-02-22 11:37:06 +0100

    changed from float to flexboxes

--- frontend/src/components/mdx/info/info.tsx
+++ frontend/src/components/mdx/info/info.tsx
@@ -5,8 +5,23 @@ import { TextField } from 'components/mdx/textField/textField';
 import React from "react";
 
 export function Info({ src, propsImage, propsText, alt, text }: { src: string, propsImage: string, propsText: string, alt: string, text: string }) {
-    return <div className="infoContainer">
-        <Image src ={src} props = { propsImage }  alt = { alt} />
-        <TextField text = {text} props = {propsText}/>
+    if(propsImage.includes("left")){
+        return <div className="infoContainer">
+            <div className="image">
+            <Image src ={src} props = { propsImage }  alt = { alt} />
+            </div>
+            <div className="text">
+            <TextField text = {text} props = {propsText}/>
+            </div>
         </div>
+    } else {
+        return <div className="infoContainer">
+            <div className="text">
+            <TextField text = {text} props = {propsText}/>
+            </div>
+            <div className="image">
+            <Image src ={src} props = { propsImage }  alt = { alt} />
+            </div>
+        </div>
+    }
   }
