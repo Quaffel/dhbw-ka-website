13705c54be622fd8386499d0cd671f5f5786bbe5
BottledSoda <linkkay123@gmail.com>
2021-02-26 16:16:31 +0100

    added responsive design

--- frontend/src/components/mdx/info/info.tsx
+++ frontend/src/components/mdx/info/info.tsx
@@ -5,23 +5,23 @@ import { TextField } from 'components/mdx/textField/textField';
 import React from "react";
 
 export function Info({ src, propsImage, propsText, alt, text }: { src: string, propsImage: string, propsText: string, alt: string, text: string }) {
-    if(propsImage.includes("left")){
-        return <div className="infoContainer">
-            <div className="image">
-            <Image src ={src} props = { propsImage }  alt = { alt} />
+        if(propsImage.includes("left")){
+            return <div className="infoContainer">
+                <div className="image">
+                <Image src ={src} props = { propsImage }  alt = { alt} />
+                </div>
+                <div className="text">
+                <TextField text = {text} props = {propsText}/>
+                </div>
             </div>
-            <div className="text">
-            <TextField text = {text} props = {propsText}/>
+        } else {
+            return <div className="infoContainer">
+                <div className="text">
+                <TextField text = {text} props = {propsText}/>
+                </div>
+                <div className="image">
+                <Image src ={src} props = { propsImage }  alt = { alt} />
+                </div>
             </div>
-        </div>
-    } else {
-        return <div className="infoContainer">
-            <div className="text">
-            <TextField text = {text} props = {propsText}/>
-            </div>
-            <div className="image">
-            <Image src ={src} props = { propsImage }  alt = { alt} />
-            </div>
-        </div>
-    }
-  }
+        }
+}
