825c8c54151fed75a0c9117c79dfb82cb204f391
Quaffel <9200663+Quaffel@users.noreply.github.com>
2021-03-23 23:34:56 +0100

    Reformat styleHelper

--- frontend/src/components/style/styleHelper.ts
+++ frontend/src/components/style/styleHelper.ts
@@ -1,36 +1,35 @@
 export function buildClassNames<S extends { [styleRuleName: string]: string }>(
-    stylesObject: S,
-    baseClasses: Array<keyof S>,
-    conditionalClasses?: Array<Array<keyof S>>,
-    conditions?: Array<boolean>
+  stylesObject: S,
+  baseClasses: Array<keyof S>,
+  conditionalClasses?: Array<Array<keyof S>>,
+  conditions?: Array<boolean>
 ): string {
-    if (!!conditionalClasses != !!conditions) {
-        throw new Error("Either both, conditionalClasses and conditions must be given or none of them");
-    }
-    if (conditions && conditions.length !== conditionalClasses?.length) {
-        throw new Error("Conditions must be of same size as conditional classes");
-    }
+  if (!!conditionalClasses != !!conditions) {
+    throw new Error("Either both, conditionalClasses and conditions must be given or none of them");
+  }
+  if (conditions && conditions.length !== conditionalClasses?.length) {
+    throw new Error("Conditions must be of same size as conditional classes");
+  }
 
-    const activeClasses = (conditionalClasses
-        // If conditional classes are given, conditions are given as well (see first assertion)
-        ?.filter((_, index) => (conditions as NonNullable<typeof conditions>)[index])
-        ?.flatMap(value => value)
-        ?.concat(baseClasses)) ?? baseClasses
+  const activeClasses = (conditionalClasses
+    // If conditional classes are given, conditions are given as well (see first assertion)
+    ?.filter((_, index) => (conditions as NonNullable<typeof conditions>)[index])
+    ?.flatMap(value => value)
+    ?.concat(baseClasses)) ?? baseClasses;
 
-    return activeClasses
-        .map(className => stylesObject[className])
-        .join(" ");
+  return activeClasses
+    .map(className => stylesObject[className])
+    .join(" ");
 }
 
-export function buildClassNamesComplex<S extends { [styleRuleName: string]: string },
-    C extends { [collectionName: string]: boolean }>(
-        stylesObject: S,
-        classCollections: { [K in keyof C]: Array<string> },
-        conditions: C
-    ): string {
-    return Object.entries(classCollections)
-        .filter(([collectionName, _]) => conditions[collectionName])
-        .flatMap(([_, collectionClassNames]) => collectionClassNames)
-        .map(className => stylesObject[className])
-        .join(" ");
+export function buildClassNamesComplex<S extends { [styleRuleName: string]: string }, C extends { [collectionName: string]: boolean }>(
+  stylesObject: S,
+  classCollections: { [K in keyof C]: Array<string> },
+  conditions: C
+): string {
+  return Object.entries(classCollections)
+    .filter(([collectionName, _]) => conditions[collectionName])
+    .flatMap(([_, collectionClassNames]) => collectionClassNames)
+    .map(className => stylesObject[className])
+    .join(" ");
 }
