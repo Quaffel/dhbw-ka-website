8bf3641bb9f6a6244496287917caa1b4564a958e
LinkKF <linkkay123@gmail.com>
2021-03-26 14:13:58 +0100

    ""

--- frontend/src/components/mdx/homescreen/homescreen.tsx
+++ frontend/src/components/mdx/homescreen/homescreen.tsx
@@ -1,13 +1,10 @@
 import './homescreen.scss';
-import { Image } from 'components/mdx/image/image';
-import { TextField } from 'components/mdx/textField/textField';
-
 import React from "react";
 
-export function Homescreen({ props, text }: { src: string, props: string, alt: string, text: string }) {
-    return <div className="homeContainer">
-        <div className="homeContainer-inside">
-            {text}
-        </div>
+export function Homescreen({  text }: { src: string, props: string, alt: string, text: string }): React.ReactElement {
+  return <div className="homeContainer">
+    <div className="homeContainer-inside">
+      {text}
     </div>
-  }
+  </div>;
+}
--- frontend/src/components/mdx/info/info.tsx
+++ frontend/src/components/mdx/info/info.tsx
@@ -4,26 +4,26 @@ import { TextField } from 'components/mdx/textField/textField';
 
 import React from "react";
 
-export function Info({ src, propsImage, propsText, alt, text, addInfo }: { src: string, propsImage: string, propsText: string, alt: string, text: string, addInfo:string }) {
-        if(propsImage.includes("left") || window.innerWidth <= 450){
-            return <div className="infoContainer">
-                <div className="additionalInfo">{addInfo}</div>
-                <div className="image">
-                <Image src ={src} props = { propsImage }  alt = { alt} />
-                </div>
-                <div className="text">
-                <TextField text = {text} props = {propsText}/>
-                </div>
-            </div>
-        } else {
-            return <div className="infoContainer">
-                <div className="text">
-                <TextField text = {text} props = {propsText}/>
-                </div>
-                <div className="image">
-                <Image src ={src} props = { propsImage }  alt = { alt} />
-                </div>
-                <div className="additionalInfo">{addInfo}</div>
-            </div>
-        }
+export function Info({ src, propsImage, propsText, alt, text, addInfo }: { src: string, propsImage: string, propsText: string, alt: string, text: string, addInfo: string }): React.ReactElement {
+  if (propsImage.includes("left")) {
+    return <div className="infoContainer-left">
+      <div className="additionalInfo">{addInfo}</div>
+      <div className="image">
+        <Image src={src} props={propsImage} alt={alt} />
+      </div>
+      <div className="text">
+        <TextField text={text} props={propsText} />
+      </div>
+    </div>;
+  } else {
+    return <div className="infoContainer-right">
+      <div className="additionalInfo">{addInfo}</div>
+      <div className="image">
+        <Image src={src} props={propsImage} alt={alt} />
+      </div>
+      <div className="text">
+        <TextField text={text} props={propsText} />
+      </div>
+    </div>;
+  }
 }
