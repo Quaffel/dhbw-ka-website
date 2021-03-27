10722d9a2f003c5408653d0f33a81b2c7db403b3
LinkKF <linkkay123@gmail.com>
2021-03-27 15:15:27 +0100

    made homescreen background variable

--- frontend/src/components/mdx/homescreen/homescreen.tsx
+++ frontend/src/components/mdx/homescreen/homescreen.tsx
@@ -1,8 +1,8 @@
 import homescreenStyles from './homescreen.module.scss';
 import React from "react";
 
-export function Homescreen({  text }: { src: string, props: string, alt: string, text: string }): React.ReactElement {
-  return <div className={`${homescreenStyles["homeContainer"]}`}>
+export function Homescreen({ src, text }: { src: string, props: string, alt: string, text: string }): React.ReactElement {
+  return <div className={`${homescreenStyles["homeContainer"]}`} style={{backgroundImage: `url(${src})`}} >
     <div className={`${homescreenStyles["homeContainer-inside"]}`}>
       {text}
     </div>
