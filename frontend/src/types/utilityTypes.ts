ef59211f9b277420583ab0eaf1cf02a6c34b2a64
Quaffel <9200663+Quaffel@users.noreply.github.com>
2021-04-01 00:28:07 +0200

    Fix type definition

--- frontend/src/types/utilityTypes.ts
+++ frontend/src/types/utilityTypes.ts
@@ -4,4 +4,4 @@ export function isNonEmptyArray<T>(arr: Array<T>): arr is NonEmptyArray<T> {
   return arr.length > 0;
 }
 
-export type Parameters<T> = T extends (...args: infer T) => any ? T : never;
+export type Parameters<T> = T extends (...args: infer T) => unknown ? T : never;
